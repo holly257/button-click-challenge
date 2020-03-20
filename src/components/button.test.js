b4e82238a49772b5d1ef913cb60f5ec6aaa8e40d
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-03-20 08:56:08 -0400

    fixed testing issue

--- src/components/button.test.js
+++ src/components/button.test.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-
-
 import Button from './Button';
 
 describe('<Button />', () => {
@@ -9,18 +7,15 @@ describe('<Button />', () => {
     it('Should call props.onClick when the button is clicked', () => {
         const mockCallBack = jest.fn();
         
-        const wrapper = shallow(<Button onClick={mockCallBackClick} className="test" text="test"/>);
+        const wrapper = shallow(<Button onClick={mockCallBack} className="test" text="test"/>);
         wrapper.find('button').simulate('click');
         expect(mockCallBack.mock.calls.length).toEqual(1);
     });
-    it('simulates click events', () => {
-        const onButtonClick = sinon.spy();
+    // it('simulates click events', () => {
+    //     const onButtonClick = sinon.spy();
 
-        const wrapper = shallow(<Button  />);
-        wrapper.find('button').simulate('click');
-        expect(onButtonClick).to.have.property('callCount', 1);
-      });
+    //     const wrapper = shallow(<Button  />);
+    //     wrapper.find('button').simulate('click');
+    //     expect(onButtonClick).to.have.property('callCount', 1);
+    //   });
 });
-
-
-// 
